ff0a811e6e3efc523f1cd68bceebc1d5ff2f2a30
Daniel Vengoechea <daniel@e-me.co>
2017-07-19 11:58:52 -0500

    Action Comany id is not required

--- src/models/actions.model.js
+++ src/models/actions.model.js
@@ -7,7 +7,7 @@ module.exports = function (app) {
   const actions = new mongooseClient.Schema({
     custom: { type: Boolean, required: true, default:false},
     name: { type: String, required: true },
-    company_id: { type: String, required: true, default:'null' },
+    company_id: { type: String, default:' ' },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
